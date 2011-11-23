5f07c4794fc436d124cb7b9cfa48ea30630ba098
Anshu Rustagi <anshu56@gmail.com>
2011-11-22 22:00:34 -0600

    Added very basic directions overlay to map

--- controlMap.js
+++ controlMap.js
@@ -21,7 +21,7 @@ function initialize() {
 
 			var jsondata = eval('(' + xmlhttp.responseText + ')');
 			for(bar in jsondata.barsArray) {
-			  var latlng = new google.maps.LatLng(jsondata.barsArray[bar].Latitude, jsondata.barsArray[bar].Longitude)
+				var latlng = new google.maps.LatLng(jsondata.barsArray[bar].Latitude, jsondata.barsArray[bar].Longitude)
 				var title2 = jsondata.barsArray[bar].barName;
 				var marker = new google.maps.Marker({
 					map : map,
@@ -33,8 +33,8 @@ function initialize() {
 				var infowindow = new google.maps.InfoWindow({
 					content : contentString
 				});
-				
-				listenmarker(marker,infowindow,map);
+
+				listenmarker(marker, infowindow, map);
 
 				//alert(results[0].geometry.location);
 			}
@@ -43,12 +43,28 @@ function initialize() {
 	xmlhttp.open("GET", "getBarsMapInfo.php", true);
 	xmlhttp.send();
 
+	var start = "510 East Clark St., Champaign, IL 61820";
+	var end = "706 South 5th St., Champaign, IL 61820";
+	var directionsDisplay = new google.maps.DirectionsRenderer();
+	var directionsService = new google.maps.DirectionsService();
+	directionsDisplay.setMap(map);
+	var request = {
+		origin : start,
+		destination : end,
+		travelMode : google.maps.TravelMode.WALKING
+	};
+	directionsService.route(request, function(result, status) {
+		if(status == google.maps.DirectionsStatus.OK) {
+			directionsDisplay.setDirections(result);
+		}
+	});
 }
-function listenmarker(marker,infowindow,map){
-		google.maps.event.addListener(marker, 'click', function() {
-					infowindow.open(map, this);
-					if(lastOpenedMarker!=null)
-						lastOpenedMarker.close();
-					lastOpenedMarker=infowindow;
-		});
+
+function listenmarker(marker, infowindow, map) {
+	google.maps.event.addListener(marker, 'click', function() {
+		infowindow.open(map, this);
+		if(lastOpenedMarker != null)
+			lastOpenedMarker.close();
+		lastOpenedMarker = infowindow;
+	});
 }
